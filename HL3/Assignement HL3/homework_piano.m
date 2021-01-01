b11f22073f3d9430365cceb50624527469080632
Alessio Lampis <58005469+AlessioLampis@users.noreply.github.com>
2021-01-01 20:56:55 +0100

    code with no loop yet

--- HL3/Assignement HL3/homework_piano.m
+++ HL3/Assignement HL3/homework_piano.m
@@ -21,6 +21,9 @@ clc
 % Temporal sampling parameters
 fs = 4*44.1e3; %[Hz] sampling frequency
 ts = 1/fs;
+duration = 8; %[s] signal length
+tSamples = duration*fs;
+t = linspace(0, duration, tSamples); %temporal vector
 
 % Fundamental note
 f1 = 52.8221; %[Hz] frequency of C2
@@ -36,18 +39,23 @@ M_s = 35e-3;    % [kg] mass of the string
 mu = M_s/L;   % [kg m^-1] avg linear mass density of the string
 eps = 7.5e-6; % [-] string stiffness parameter assumed equal to kappa- string stiff. coefficient
 c = sqrt(T0/mu);    % [m/s] string wave velocity
+x0 = 0.12*L; %[m] stricking position (see d/L from Fletcher)
 
 % Aliasing condition
+if (fs < 2*f1)
+    disp("WARNING: SAMPLING FREQUENCY BELOW NYQUIST");
+end
 
 % Spatial sampling parameters
 X_max = sqrt( 0.5*((c*ts)^2 + 4*b2*ts + sqrt( ((c*ts)^2 + 4*b2*ts)^2 + 16*(eps*ts)^2 )) ); %stability condition (see Saitis)
 
 % Number of maximum spatial steps
 gamma = fs/(2*f1);
+
+% Integer values
 N_max = ceil(sqrt( (-1 + sqrt(1 + 16*eps*(gamma^2)))/(8*eps) ));
 X_min = L/N_max;
 
-% Integer values
 % Spatial sampling
 N = N_max - 1;
 X = L/N;    %as close as possible to Xmin (see Chaigne et al.)
@@ -55,6 +63,8 @@ X = L/N;    %as close as possible to Xmin (see Chaigne et al.)
 if (X < X_min | X > X_max )
     display("WARNING: WRONG SPATIAL SAMPLING STEP");
 end
+
+x = linspace( 0 , L , X); %spatial vector
 
 % FD parameters
 lambda = c*ts/X; %Courant parameter
@@ -66,6 +76,8 @@ Vh0 = 2.5;  % [m/s] initial hammer velocity
 af = (ts^2/mu)/(1 + b1*ts); %force coefficient
 bh = 1e-4; %[s^-1] fluid damping coefficient
 Mh = 4.9e-3; %[Kg] hammer mass
+eta = zeros(length(t)); %hammer displacement vector
+Fh = zeros(length(t)); %hammer force vector
 
 % Hammer contact window definition
 
@@ -84,10 +96,24 @@ d2 = (-1 + bh*ts/(2*Mh))/(1 + bh*T/(2*Mh));
 df = (-ts^2/Mh)/(1+bh*ts/(2*Mh));
 
 % Bridge boundary coefficients
-zeta_b = 1000; 
+zeta_b = 1000; %normalized impedance at the bridge
+
+%bri coefficients
+br1 = (2 - 2*lambda^2*mi - 2*lambda^2)/(1 + b1*ts + zeta_b*lambda);
+br2 = (4*lambda^2*mi + 2*lambda^2)/(1 + b1*ts + zeta_b*lambda);
+br3 = (-2*lambda^2*mi)/(1 + b1*ts + zeta_b*lambda);
+br4 = (-1 -b1*ts + zeta_b*lambda)/(1 + b1*ts + zeta_b*lambda);
+br5 = (ts^2/mu)/(1 + b1*ts + zeta_b*lambda);
 
 % Left hand (hinged string end) boundary coefficients
-zeta_l = 1e20;
+zeta_l = 1e20; %normalized impedance
+
+%bli coefficients
+bl1 = (2 - 2*lambda^2*mi - 2*lambda^2)/(1 + b1*ts + zeta_l*lambda);
+bl2 = (4*lambda^2*mi + 2*lambda^2)/(1 + b1*ts + zeta_l*lambda);
+bl3 = (-2*lambda^2*mi)/(1 + b1*ts + zeta_l*lambda);
+bl4 = (-1 -b1*ts + zeta_l*lambda)/(1 + b1*ts + zeta_l*lambda);
+bl5 = (ts^2/mu)/(1 + b1*ts + zeta_l*lambda);
 
 % Hammer felt parameters
 p = 2.3; 
@@ -96,8 +122,18 @@ K = 4e8;
 
 %% Computation of the FD scheme
 % Initialization
+y = zeros(length(t), length(x));
 
 % Computation loop
+for n = 1:length(x)
+    %boundary conditions
+    
+    for m = 1:length(t)
+    %initial conditions
+        
+    end
+end
+
 %% Plot the displacement in time
 
 %% Plot the synthesized signal play it and save it on the disk
