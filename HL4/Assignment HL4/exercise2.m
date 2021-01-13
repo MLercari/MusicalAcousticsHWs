a5f4661b3ccb1b2c43f72a5981039ca8f0d873e7
Alessio Lampis <58005469+AlessioLampis@users.noreply.github.com>
2021-01-13 12:55:33 +0100

    boh

--- HL4/Assignment HL4/exercise2.m
+++ HL4/Assignment HL4/exercise2.m
@@ -18,34 +18,39 @@ clc
 % Collecting autocorrelations and plotting them in order to analyze
 % reflections. Try different takes and input signals
 
-nMic = % Number of microphones
-typeOfSignal = % Noise or sweep
-dir =       % Recordings directory
+nMic = 1;% Number of microphones
+typeOfSignal = ["sweep" , "noise"]; % Noise or sweep
+dir =  "Recordings";     % Recordings directory
 
 speed_of_sound = 343.8; % [m]/[s]
 
 %% Plot the signal autocorrelation
 figure;
-t = tiledlayout('flow');
-
+%t = tiledlayout('flow');
 for n = 1:nMic
     % Load the signal
-       % Time length
+    [x, Fs] = audioread(strcat(dir,'/', typeOfSignal(2), '/', '1.wav' ));
+    % Time length
+    tlen = 4; %[s] duration of the signal
+    tsamples = length(x);
     % Time axis
-    % Auto correlation
-    Half of the autocorrelation
+    t = linspace(0, tlen, tsamples)';
+    % Auto correlation 
+    xc = xcorr(x);
+    xc(tsamples+1:end) = [];
     % Plot the autocorrelation
     nexttile
     plot(t, xc);
     title(['Mic: ', num2str(n)]);
     axis([0 0.02 -1 1]);    % Limit the axis
     xlabel('Time (sec)');
+    
 end
 
 %% MIC TO REFLECTORS DISTANCE COMPUTATION
 % Put here the difference between first reflection 
-delay = %[s]
+%delay = %[s]
 
-distance = ;%[m]
+%distance = ;%[m]
 
 fprintf(sprintf('Average distance between mic and first reflection %f m\n', distance));
